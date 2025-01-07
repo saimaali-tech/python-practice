4efd7f668b334ea9b24a7e195553e51770b55510
salmanale <salmankareem182@gmail.com>
2025-01-07 13:05:03 +0500

    first commit

---
+++ conditionalstatements.py
@@ -0,0 +1,46 @@
+
+# if statement in python
+# a=int(input("enter a number"))
+# if a%2==0:
+#     print (a ,"even number")
+
+
+# if else statement in python
+# a=int(input("enter a number"))
+# if a%2==0:
+#     print (a ,"even number")
+# else:
+#     print(a, "odd number")
+
+# if elif statements in python
+# per=int(input("enter a number"))
+# if per>=70:
+#     print("first division")
+# elif per>=60:
+#     print("second division")
+# elif per>=50:
+#     print("third division")
+# else:
+#     print('fail')
+
+num=int(input("enter the number"))
+if num>=80:
+    print("A+")
+elif num>=70:
+    print("B")
+elif num>=40:
+    print("C")
+elif num>=30:
+    print("D") 
+else:
+    print("fail") 
+
+                  
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,107 @@
+# my first python program
+# print("hello saima")
+
+
+# arithematic operators
+
+# a=12
+# b=67
+# print(a+b) #sum
+# print(a*b) #multiplication
+# print(a/b) #division
+# print(a%b) #remainder
+# print(a**b) #a^b
+  
+
+# relational operators
+
+# print(a==b) #false
+# print(a!=b) #true
+# print(a<=b) #true
+# print(a>=b) #false
+
+
+#assignment operators
+
+# value=20
+# value+=20
+# print(value)
+# num=35
+# num-=35
+# print(num)
+
+
+#logical operators
+
+# val1=True
+# val2=False
+# print(val1 and val2)
+# print(val1 or val2)
+# print(val2 and val1)
+
+# conversion where the python automatically change the data type
+
+# a=12
+# b=34.7
+# sum=a+b
+# print(sum)
+# print(type(sum))
+
+# type casting where programmer mannually change the data type
+
+# a=int("58")
+# b=100
+# sum=a+b
+# print(sum)
+# print(type(a))
+# print(type(sum))
+
+
+#input in python
+# name=input("what is your name:")
+# age=int(input("what is your age :"))
+# print("welcome",name  ,"your age is ",age)
+# print(type(age))
+# a=int(input("enter a value :"))
+# b=int(input("enter the next value :"))
+# print("sum is",a+b)
+
+
+# varaible cancatenation
+
+a= '''hello world
+gfhfhf
+hghjgjhg
+hghgfhf
+'''
+
+print(a)
+# c=34
+# print(34+c)
+
+
+# membership operators in python
+
+# string1="hello world"
+# print("h" in string1)
+# f=[13,24,45,67,78]
+# print(50 not in f)
+
+
+# identity operators
+
+# x=23
+# y=67
+# print(x is not y)
+
+# bitwise operators in python
+# x=17
+# y=18
+# print (bin(x))
+# print(bin(y))
+# print(bin(x&y),(x&y))  
+# print(bin(x|y),(x|y))
+# print(bin(x^y),(x^y))
+
+
+
